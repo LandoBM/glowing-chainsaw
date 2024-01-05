2b75a8686f1d588ed00b443349324db6db9bf607
Landon Murray <landonbmurray@icloud.com>
2024-01-04 19:28:34 -0500

    added gitignore for env

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,11 +1,22 @@
-const { connect, connection } = require('mongoose')
+// const { connect, connection } = require('mongoose')
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/newApp'
+// const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/newApp'
 
-connect(connectionString, {
-    useNewURLParser: true,
-    useUnifiedTopology: true,
-});
+// connect(connectionString, {
+//     useNewURLParser: true,
+//     useUnifiedTopology: true,
+// });
 
-module.exports = connection
+// module.exports = connection
 
+require('dotenv').config()
+const Sequelize =  require('sequelize')
+
+
+const sequelize = new Sequelize (
+    process.env.JAWSDB_URL, 
+    {
+        dialect: 'mysql'
+    })
+
+module.exports = sequelize
